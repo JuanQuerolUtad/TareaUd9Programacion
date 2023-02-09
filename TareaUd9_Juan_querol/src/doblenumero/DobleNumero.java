9a5142511c3542f52644ff8383bcb46107329919
Juan Querol Leon <juan.querol@live.u-tad.com>
2023-02-09 14:01:45 +0100

    DobleNumero terminado

---
+++ TareaUd9_Juan_querol/src/doblenumero/DobleNumero.java
@@ -0,0 +1,19 @@
+package doblenumero;
+
+public class DobleNumero {
+	private int num;
+	
+	public DobleNumero(int num) {
+		this.num=num;
+		
+	}
+
+	public int getValue() {
+		return num*2;
+	}
+
+	public void setValue(int num) {
+		this.num = num;
+	}
+	
+}
---
+++ TareaUd9_Juan_querol/src/doblenumero/PruebaDobleNumero.java
@@ -0,0 +1,14 @@
+package doblenumero;
+
+public class PruebaDobleNumero {
+
+	public static void main(String[] args) {
+		DobleNumero x=new DobleNumero(1);
+		System.out.println(x.getValue());
+		x.setValue(8);
+		System.out.println(x.getValue());
+				
+
+	}
+
+}
